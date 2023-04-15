3c9b36a8a2e4f4b2c8efa63dc02b8ad6e102aff1
Anastasia15975 <anonim15975@gmail.com>
2023-04-15 09:38:31 +0300

    добавлние новой музыки

--- src/main/java/com/example/playlist/HelloController.java
+++ src/main/java/com/example/playlist/HelloController.java
@@ -2,13 +2,13 @@ package com.example.playlist;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HelloController {
     @FXML
@@ -26,7 +26,17 @@ public class HelloController {
     @FXML
     private TableColumn<Music, String> singerColumn;
 
+    @FXML
+    private TextField albumField;
 
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField singerField;
+
+    @FXML
+    private Button btnAdd;
     Connection connection;
 
     public void initialize() throws SQLException, ClassNotFoundException {
@@ -38,5 +48,35 @@ public class HelloController {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         singerColumn.setCellValueFactory(new PropertyValueFactory<>("singer"));
         albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
+    }
+
+    public void addMusic() throws SQLException, ClassNotFoundException {
+        String name, singer, album;
+
+        name = nameField.getText();
+        singer = singerField.getText();
+        album = albumField.getText();
+
+        DBConnector connector = new DBConnector();
+        connection = connector.getConnection();
+        Statement statement = connection.createStatement();
+
+        //добавление исполнителя
+        statement.executeUpdate("INSERT INTO singer(singer.name) VALUES ('" + singer + "')");
+        //берём его id
+        ResultSet setSinger = statement.executeQuery("SELECT singer.id FROM singer WHERE singer.name = '" + singer + "'");
+        setSinger.next();
+        int idSinger = setSinger.getInt("singer.id");
+
+        //добавление альбома
+        statement.executeUpdate("INSERT INTO album(album.name) VALUES ('" + album + "')");
+        //берём его id
+        ResultSet setAlbum = statement.executeQuery("SELECT album.id FROM album WHERE album.name = '" + album + "'");
+        setAlbum.next();
+        int idAlbum = setAlbum.getInt("album.id");
+
+        //добавление и вывод новой песни
+        statement.executeUpdate("INSERT INTO music(music.name, music.singerId, music.albumId) VALUES ('" + name + "'," + idSinger + "," + idAlbum + ")");
+        initialize();
     }
 }
